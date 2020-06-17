6bf3744b78d5ca556f0e2723c724ac49eb1529b8
Kristian Skog <skogkristian@gmail.com>
2020-06-18 00:40:58 +0200

    Added new feature edit task and code refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 (function() {
 
-    let toDoList;
+    let toDoList, findIndexToRemove;
     const getInput = document.querySelector(".addItem");
     const getAddButton = document.querySelector(".addItemButton");
     const getTodoList = document.querySelector(".toDoList");
@@ -25,10 +25,13 @@
             let newListItem = `
             <li class="item" id=${toDoList[i].id}>
                 <div class="item__container">
+                <input class="task__input display-none" type="text">
                 <p class="task">${toDoList[i].name}</p>
                 <span class="date">${toDoList[i].date}</span>
                 </div>
-                <button class="removeButton">Remove</button>    
+                <div class="removeButton__container">
+                    <button class="removeButton">Remove</button>    
+                </div>
             </li>
             `;
             getTodoList.insertAdjacentHTML("afterbegin", newListItem);
@@ -36,12 +39,12 @@
     }
 
     const createdDateOfTodo = () => {
-        let createDate = new Date();
-        let year = createDate.getFullYear();
-        let months = createDate.getMonth();
-        let day = createDate.getDay();
-        let hours = createDate.getHours();
-        let min = createDate.getMinutes();
+        let createDate = new Date(),
+        year = createDate.getFullYear(),
+        months = createDate.getMonth(),
+        day = createDate.getDay(),
+        hours = createDate.getHours(),
+        min = createDate.getMinutes();
 
         if (months < 10) {
             months = "0" + months;
@@ -61,17 +64,9 @@
 
     const removeItemFromList = (event) => {
         if (event.target.className === "removeButton") {
-            let getItemId = event.target.closest(".item").id;
-            getItemId = parseFloat(getItemId);
-            var findIndexToRemove = toDoList
-                .map(function(item) {
-                    return item.id;
-                })
-                .indexOf(getItemId);
+            findArrayItemId(event);
             toDoList.splice(findIndexToRemove, 1);
-
-            printTodoList();
-            saveTodoInLocalStorage();
+            saveTodo();
         }
     }
 
@@ -88,17 +83,60 @@
         }
     }
 
+    const findArrayItemId = (event) => {
+        let getItemId = event.target.closest(".item").id;
+        getItemId = parseFloat(getItemId);
+        findIndexToRemove = toDoList
+            .map(function(item) {
+                return item.id;
+            })
+            .indexOf(getItemId);
+    }
+
+    const changeInputValue = (event) => {
+        if (event.target.className === "task") {
+            const getTaskId = event.target.closest('.item').id;
+            const getTaskText = event.target;
+            const getTaskInput = event.target.closest('.item').querySelector('.task__input');
+            const getTaskTextValue = event.toElement.textContent;
+
+            getTaskText.classList.add('display-none');
+            getTaskInput.classList.remove('display-none');
+            getTaskInput.value = getTaskTextValue;
+
+            findArrayItemId(event);
+
+            getTaskInput.addEventListener('keydown', function(event) {
+                if (event.keyCode === 13) {
+                    event.preventDefault();
+                    getTaskText.classList.remove('display-none');
+                    getTaskInput.classList.add('display-none');
+                    toDoList[findIndexToRemove].name = this.value;
+
+                    saveTodo();
+                }
+            });
+        }
+    }
+
+    const saveTodo = () => {
+        printTodoList();
+        saveTodoInLocalStorage();
+    }
+
     getAddButton.addEventListener("click", function() {
         addItemToTodo();
         printTodoList();
     });
-    window.addEventListener("keydown", function(event) {
+    getInput.addEventListener("keydown", function(event) {
         if (event.key === "Enter") {
             addItemToTodo();
             printTodoList();
         }
     });
     getTodoList.addEventListener("click", removeItemFromList);
+
+    document.addEventListener('dblclick', changeInputValue);
 
     getTodoFromLocalStorage();
     printTodoList();
